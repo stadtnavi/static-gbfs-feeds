3042728af7a28a1043517ad952ed62c7270aa7a5
Beck-berry <trebeka98@gmail.com>
2020-06-29 14:46:07 +0200

    update script and status file

--- scripts/geojsonToStationInfo.py
+++ scripts/geojsonToStationInfo.py
@@ -1,16 +1,17 @@
-# Run as: ENV_SOURCE=<path to source .geojson> ENV_DEST=<path to destination .json> python geojsonToStatus.py
+# Run as: ENV_SOURCE=<path to source .geojson> ENV_DEST=<path to destination folder> python geojsonToStatus.py
 
 import json
 import os
 from datetime import datetime
 
 sourceFile = os.getenv('ENV_SOURCE')
-destFile = os.getenv('ENV_DEST')
+destFolder = os.getenv('ENV_DEST')
 
 with open(sourceFile) as source:
     source_data = json.load(source)
 
-stations = []
+info = []
+status = []
 
 i = 0
 for feature in source_data["features"]:
@@ -18,21 +19,35 @@ for feature in source_data["features"]:
 		name = feature['properties']['name_de']
 	else:
 		name = feature['properties']['name']
-	stations.append({
+	info.append({
 		"lat": feature["geometry"]["coordinates"][1],
 		"lon": feature["geometry"]["coordinates"][0],
 		'name': name,
 		'station_id': str(i + 1)
 	})
+	status.append({
+		"num_cars_available": 0,
+		"is_renting": True,
+		'station_id': str(i + 1)
+	})
 	i += 1
 
-created_data = {
+created_info = {
 	"data": {
-		"stations": stations
+		"stations": info
 	},
 	"last_updated": int(datetime.timestamp(datetime.now())),
 	"ttl": 60000
 }
+with open(destFolder + "/station_information.json", "w") as dest:
+	json.dump(created_info, dest, indent=2)
 
-with open(destFile, "w") as dest:
-	json.dump(created_data, dest, indent=2)
+created_status = {
+	"data": {
+		"stations": status
+	},
+	"last_updated": int(datetime.timestamp(datetime.now())),
+	"ttl": 60000
+}
+with open(destFolder + "/station_status.json", "w") as dest:
+	json.dump(created_status, dest, indent=2)
